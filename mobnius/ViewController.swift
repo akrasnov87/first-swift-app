5e3362d69461d1504d9aa48ae419ef5c1cd29409
Александр Краснов <akrasnov87@gmail.com>
2021-04-06 15:39:05 +0300

    Initial Commit

---
+++ mobnius/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobnius
+//
+//  Created by Александр Краснов on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
